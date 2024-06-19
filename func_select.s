fefd5b34a05afb534ec2617a18c54787e7ffbba4
Tamir Ashwal <144041557+TamirAshwal@users.noreply.github.com>
2024-06-19 14:18:46 +0300

    Add files via upload

--- func_select.s
+++ func_select.s
@@ -9,13 +9,19 @@ case_33_print:
 case_34_print:
     .string "in case 34 - pstrijcpy "
 case_invalid_input_print:
-    .string "case: invalid input \n"
+    .string "invalid option!\n"
 default_print:
     .string "default"
 pstrlen_message:
     .string "first pstring length: %d, second pstring length: %d\n"
+length_message:  
+    .string "length: %d, "
 swapcase_message:
     .string "string: %s\n"
+case_34_user_format:
+    .string " %d %d"
+case_34_check:
+    .string "this is the first index: %d, this is the second %d\n"
 .L4:
     .quad .case_31 # case 31 - pstrlen
     .quad .case_32 # invalid input 
@@ -38,56 +44,53 @@ run_func:
         call printf
         jmp .done
     .case_31:
-        subq $8, %rsp #align the stack becuse we only push 1 value to the stack
-        pushq %rsi # push the first pstring to the stack
+        movq %rsi, %rdi #move the first pstring to rdi
         xorq %rax, %rax
         call pstrlen #call pstrlen
         movq %rax, %rsi
-        subq $8, %rsp #align the stack becuse we only push 1 value to the stack
-        pushq %rdx # push the second pstring to the stack
+        movq %rdx, %rdi #move the second pstring to rdi
         xorq %rax, %rax
         call pstrlen #call pstrlen
         movq %rax, %rdx
+        xorq %rax, %rax
         movq $pstrlen_message, %rdi #print the message
         call printf
         jmp .done #exit the function
     .case_32:
         jmp .invalid_input
     .case_33:
-    movq %rdx, %r13
-
-        subq $8, %rsp
-        pushq %rsi
-        xorq %rax, %rax
-        call pstrlen
-        pushq %rsi
-        pushq %rax
+        movq %rdx, %r13 # save the second pstring
+        movq %rsi, %rdi # move the first pstring to rdi
         xorq %rax, %rax
         call swapCase
         movq %rax, %rsi
         movq $swapcase_message, %rdi
         xorq %rax, %rax
         call printf
-        movq %r13, %rsi
-        subq $8, %rsp
-        pushq %rsi
-        xorq %rax, %rax
-        xorq %rdi, %rdi
-        xorq %rdx, %rdx
-        call pstrlen
-        pushq %rsi
-        pushq %rax
+        movq %r13, %rdi # move the second pstring to rdi
         xorq %rax, %rax
         call swapCase
         movq %rax, %rsi
         movq $swapcase_message, %rdi
         xorq %rax, %rax
         call printf
-        jmp .done
+        jmp .done #exit the function
     .case_34:
-        movq $case_34_print, %rdi
+        subq $16, %rsp
+        movq %rsi, %r12
+        movq %rdx, %r13
+        leaq -8(%rbp), %rsi
+        leaq -16(%rbp), %rdx
+        movl $case_34_user_format, %edi
         xorq %rax, %rax
-        call printf
+        call scanf
+        movzbl -8(%rbp), %esi
+        movzbl -16(%rbp), %edx
+        movl %edx, %ecx
+        movl %esi, %edx
+        movq %r12, %rdi
+        movq %r13, %rsi
+        call pstrijcpy
         jmp .done
     .done:
     movq %rbp, %rsp
--- pstring.s
+++ pstring.s
@@ -3,14 +3,18 @@
 .section .rodata
 length_message:  
     .string "length: %d, "
+invalid_input_message:
+    .string "invalid input!\nlength: %d, string: %s\nlength: %d, string: %s\n"
+print_new_string:
+    .string "length: %d, string: %s\nlength: %d, string: %s\n"
+
 .section .text
 .type  pstrlen, @function
 .globl pstrlen
 pstrlen:
 pushq %rbp
 movq %rsp, %rbp
-movq 0x10(%rbp), %rdi
-movzbl (%rdi), %eax
+movzbl (%rdi), %eax # move the length to %eax
 movq %rbp, %rsp
 popq %rbp
 ret
@@ -19,48 +23,88 @@ ret
 swapCase:
 pushq %rbp
 movq %rsp, %rbp
-movq 0x10(%rbp), %rsi # get the length of the string
+movq %rdi, %r14 # save the address of the string
+xorq %rax, %rax
+call pstrlen # call pstrlen to get the length of the string
+movq %rax, %rsi # move the length to rsi to print it
+movq %rax, %r12 # move the length to r12 save the length 
 xorq %rax, %rax
 movq $length_message, %rdi
-call printf
-movq 0x10(%rbp), %rcx # the length of the string is in rcx
-movq 0x18(%rbp), %rsi # get the string
+call printf # print the legnth 
+movq %r14, %rsi # move the address of the string to rdi
+movq %r12, %rcx # move the length to rcx to use it in the loop
 leaq 1(%rsi), %rsi # go the the beginning of the string
-movq %rsi, %rdi
+movq %rsi, %rdi 
 .loop:
-    cmpq $0, %rcx
-    je .end
-    movb (%rsi), %al
-    cmpb $65, %al
-    jb .next_letter
-    cmpb $122, %al
-    ja .next_letter
-    cmpb $90, %al
-    jbe .upper_to_lower
-    cmpb $97, %al
-    jae .lower_t0_upper
-    jmp .next_letter
+    cmpq $0, %rcx # compare rcx which has the length of the string with 0 
+    je .end # is rcx is 0 we finished going through the string
+    movb (%rsi), %al # get the character
+    cmpb $65, %al # compare the character with 65
+    jb .next_letter # if the character is less than 65 we go to the next letter
+    cmpb $122, %al # compare the character with 122
+    ja .next_letter # if the character is greater than 122 we go to the next letter
+    cmpb $90, %al # compare the character with 90
+    jbe .upper_to_lower # if the character is less than or equal to 90 it's is capital letter so go to upper to lower
+    cmpb $97, %al # compare the character with 97
+    jae .lower_t0_upper # if the character is greater than or equal to 97 it's is small letter so go to lower to upper
+    jmp .next_letter # if the character is not a letter go to the next letter
     .upper_to_lower:
-        addb $32, %al
-        movb %al, (%rsi)
-        jmp .next_letter
+        addb $32, %al # add 32 to convert the upper case to lower case
+        movb %al, (%rsi) # put the charcter in the string
+        jmp .next_letter # go to the next letter
     .lower_t0_upper:
-        subb $32, %al
-        movb %al, (%rsi)
-        jmp .next_letter
+        subb $32, %al  # subtract 32 to convert the lower case to upper case
+        movb %al, (%rsi) # put the charcter in the string
+        jmp .next_letter # go to the next letter
     .next_letter:
-        incq %rsi
-        decq %rcx
-        jmp .loop
+        incq %rsi # go to the next letter
+        decq %rcx # decrease the length of the string
+        jmp .loop # go to the next letter
+jmp .end
+.type  pstrijcpy, @function
+.globl pstrijcpy
+pstrijcpy:
+pushq %rbp
+movq %rsp, %rbp
+xorq %rax, %rax
+pushq %rsi
+pushq %rdi
+movzbl (%rdi), %r12d # save the length of the first pstring in r8
+movzbl (%rsi), %r13d # save the length of the second pstring in r9
+leaq 1(%rdi), %rdi # go to the first pstring
+leaq 1(%rsi), %rsi # go to the second pstring
+cmpq %r8, %rcx
+jb .invalid_input
+cmpq %r9, %rcx
+jb .invalid_input
+jmp .valid_input
+.valid_input:
+addq %rcx, %rdi # go to the index in the string
+addq %rcx, %rsi #go to the index in the string
+.loop_copy:
+cmpq %rcx, %r8
+ja .new_string
 
-#movzbl (%rdi), %ecx
-#leaq 1(%rsi), %rsi # go the the beginning of the string
-#leaq 1(%rsi), %rax
-#movb (%rsi), %al
-#subb $32, %al
+
+incq %rcx
+jmp .loop_copy
+.new_string:
+
+jmp .end
 
 
-    
+.invalid_input:
+movq $invalid_input_message, %rdi
+movl %r12d, %esi
+movl %r13d, %ecx
+popq %rdx
+popq %r8
+leaq 1(%rdx), %rdx
+leaq 1(%r8), %r8
+xorq %rax, %rax
+call printf
+jmp .end
+
 .end:
 movq %rdi, %rax
     movq %rbp, %rsp
